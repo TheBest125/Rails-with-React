4598692d300e154f9ed62f8884ba491b2fe7f707
kevinbruyne0125 <kevinbruyne0125@gmail.com>
2021-05-30 16:09:11 -0500

    Created Database Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,26 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+1.times do |index|
+    users = User.create!(
+        firstname: 'Ashkan',
+        lastname: 'Ebtekari',
+        phone: '347325654',
+        email: 'chamepp@star.com',
+        password: 'chameppstar',
+        password_confirmation: 'chameppstar'
+    )
+end
+puts('Created User Successfully !')
+
+
+20.times do |index|
+    products = Product.create!(
+        number: index,
+        title: "Title #{index}",
+        price: "#{index}",
+        category: "Herbal",
+        description: "This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."
+    )
+end
+puts('Created Products Successfully')
