e71a59dd8a02dd3c3f784355214baad831e2a8d4
kevinbruyne0125 <kevinbruyne0125@gmail.com>
2021-05-30 16:07:27 -0500

    Updated Index

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,18 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { render } from "react-dom";
+import { useHistory } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
+import AutoRender from "../components/public/AutoRender";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import { store, persistor } from "../Redux/store";
 
 
-
 document.addEventListener("DOMContentLoaded", () => {
-  render(
+
+  render (
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <App />
@@ -21,5 +23,4 @@ document.addEventListener("DOMContentLoaded", () => {
     document.body.appendChild(document.createElement("div"))
   );
 });
-
 export default App;
