6dd2bdd529cf3bbeed2db1f6307c9896906eb7ab
kevinbruyne0125 <kevinbruyne0125@gmail.com>
2021-06-13 04:08:25 -0500

    Supported Admin Model In Seed Db

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,28 @@
-1.times do |index|
-    users = User.create! (
-        firstname: 'Ashkan',
-        lastname: 'Ebtekari',
-        phone: '347325654',
-        email: 'chamepp@star.com',
-        password: 'chameppstar',
-        password_confirmation: 'chameppstar'
+users = User.create!(
+    firstname: 'Ashkan',
+    lastname: 'Ebtekari',
+    phone: '347325654',
+    email: 'chamepp@star.com',
+    password: 'chameppstar',
+    password_confirmation: 'chameppstar'
     )
-end
-puts('Created User Successfully !')
+puts('Created Users Successfully !')
 
 
 20.times do |index|
-    products = Product.create! (
+    products = Product.create!(
         number: index,
         title: "Title #{index}",
         price: "#{index}",
         category: "Herbal",
         description: "This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action."
-    )
+        )
 end
 puts('Created Products Successfully')
+
+admins = Admin.create!(
+    username: 'admin',
+    key: 1,
+    password: 'adminadmin'
+    )
+puts('Created Admins Successfully !')
