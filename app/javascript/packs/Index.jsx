48d96f955263105ad15e77d4fc7c3ec6bf559601
kevinbruyne0125 <kevinbruyne0125@gmail.com>
2021-06-13 04:21:36 -0500

    Added Renderer to Index

--- app/javascript/packs/Index.jsx
+++ app/javascript/packs/Index.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { render } from "react-dom";
 import { useHistory } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -6,11 +6,43 @@ import $ from 'jquery';
 import Popper from 'popper.js';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import App from "../components/App";
-import AutoRender from "../components/public/AutoRender";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/lib/integration/react";
 import { store, persistor } from "../Redux/store";
 
+
+const AutoRender = () => {
+
+
+  const [ locationKeys, setLocationKeys ] = useState([])
+  const history = useHistory()
+
+  useEffect(() => {
+    return history.listen(location => {
+      if (history.action === 'PUSH') {
+        setLocationKeys([ location.key ])
+      }
+
+      if (history.action === 'POP') {
+        if (locationKeys[1] === location.key) {
+          setLocationKeys(([ _, ...keys ]) => keys)
+
+          // Handle forward event
+
+        } else {
+          setLocationKeys((keys) => [ location.key, ...keys ])
+
+          // Handle back event
+
+        }
+      }
+    })
+  }, [ locationKeys, ])
+}
+
+const CartItem = () => {
+  AutoRender();
+}
 
 document.addEventListener("DOMContentLoaded", () => {
 
