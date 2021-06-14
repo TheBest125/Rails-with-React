d5af2c095bc06a60f984dbe182ae02076385f36d
kevinbruyne0125 <kevinbruyne0125@gmail.com>
2021-06-14 16:02:10 -0500

    Added Image Validation

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,6 @@ class Product < ApplicationRecord
     validates :number, :title, :category, presence: true
     validates :price, presence: true
     validates :description, presence: true,  length: { minimum: 20, maximum: 200 }
+    validates :image, presence: true
     has_one_attached :image
 end
