e00b5e71a69f69dcd60af5814a12f6db779414ae
kevinbruyne0125 <kevinbruyne0125@gmail.com>
2021-06-27 09:39:12 -0500

    added image parameter

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,4 +4,12 @@ class Product < ApplicationRecord
     validates :description, presence: true,  length: { minimum: 20, maximum: 200 }
     validates :image, presence: true
     has_one_attached :image
+
+  # Get Image
+  def image_url
+    Rails.application
+                    .routes
+                    .url_helpers
+                    .rails_blob_path(image, :only_path => true)
+  end
 end
